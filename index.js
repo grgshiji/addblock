cbe5cc437a39967674128c05974bb521d3cc7023
grgshiji <shiji@neoito.com>
2023-01-27 10:41:44 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const addblock = async (req, res) => {
     res.write(JSON.stringify({success: true, msg: "Health check success"}))
     res.end()
   }
-
+  const bodyBuffer = [];
+  for await (const chunk of req) {
+    bodyBuffer.push(chunk);
+  }
+  const data = Buffer.concat(bodyBuffer).toString();
+  const reqData =  JSON.parse(data || "{}");
+  const sum = reqData.num1 + reqData.num2
   // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello addblock`}))
+  res.write(JSON.stringify({success: true, msg: `Sum of two numbers`, sum }))
   res.end()
   
 }
